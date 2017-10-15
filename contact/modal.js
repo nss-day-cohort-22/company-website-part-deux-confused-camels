f6d7a7453073aba99d7d4c118974c56e31e1c334
Christopher Miller <camiller.yr@gmail.com>
2017-10-15 18:57:28 -0500

    Basic framework for comment / email section

---
+++ contact/modal.js
@@ -0,0 +1,30 @@
+//Login Validation
+login = function() {
+    if (document.getElementById("adminLogin").elements[0].value === "camiller" && document.getElementById("adminLogin").elements[1].value === "password") {
+        document.getElementById("modalTitle").innerHTML = "Emails"
+        document.getElementById("modalContent").innerHTML = ""
+        displayMessages("email", "modalContent")
+    } else {
+        document.getElementById("loginFailure").innerHTML = "Username or Password are invalid"
+    }
+}
+
+//Modal functionality
+modal = function() {
+    document.getElementById('modal').style.display = "block"
+
+    document.getElementById("modalTitle").innerHTML = "Admin Login"
+
+    document.getElementById("modalContent").innerHTML = `
+    <form id="adminLogin">
+        <input type="text" name="userName" id="userName" value="User Name">
+        <input type="text" name="password" id="password" value="Password">
+    </form>
+    <button onclick="login()">Login</button>
+    <div id="loginFailure"></div>
+    `
+}
+
+document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementById('modal').style.display = "none";
+}
