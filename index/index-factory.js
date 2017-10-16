1f6ab7e948c1db3718289f0e970c57253cf7ecbb
John Dulaney <j@Johns-MacBook-Pro.local>
2017-10-16 14:22:18 -0500

    changed to camels

--- index/index-factory.js
+++ index/index-factory.js
@@ -3,7 +3,7 @@
   let homePage = {
     "picture": "./index/images/chicken_sunglasses_b.jpg",
     "button": "./products/products.html",
-    "title": "Welcome to Camel Chicken Sunglasses!",
+    "title": "Welcome to Camels Chicken Sunglasses!",
     "body": "We are CCS. The only source for modern day trendy eyewear for the hipster chick that doesnt want to go into a baby boomer blood rage. Our products are 100% made in the US and set the trends for all of the industry. Whether you're a rooster or a hen, we have a style to match your needs. Click on the button to get started!",    
     "video": "https://www.youtube.com/embed/5xQD5G10W4U",
   }
