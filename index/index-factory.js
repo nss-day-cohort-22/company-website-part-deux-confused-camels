23e65061cd88c375d66bef83490ed303701b3512
John Dulaney <j@Johns-MacBook-Pro.local>
2017-10-16 12:12:12 -0500

    added some content to body/footer

--- index/index-factory.js
+++ index/index-factory.js
@@ -1,22 +1,17 @@
 //Database entries for use with index-controller.js
 //Stringified and stored below
-
   let homePage = {
     "picture": "./index/images/chicken_sunglasses_b.jpg",
     "button": "./products/products.html",
-    "title": "PLACEHOLDER",
-    "body": "Pop-up helvetica edison bulb, godard trust fund etsy bushwick asymmetrical fingerstache palo santo truffaut tilde man bun four loko glossier. Tilde taxidermy selvage woke salvia you probably haven't heard of them cred chillwave. Gentrify trust fund bushwick, four loko hexagon yr echo park raclette kombucha offal mlkshk pug. Tote bag banh mi marfa chambray vice thundercats enamel pin, flexitarian lyft man bun farm-to-table. Squid umami aesthetic cred, post-ironic austin kickstarter fanny pack readymade mumblecore hot chicken roof party street art food truck fam. Leggings cloud bread small batch knausgaard cronut celiac jianbing. Ugh unicorn sriracha trust fund cloud bread fashion axe bushwick. Echo park shaman tumblr actually lomo. Fashion axe semiotics kogi keytar, typewriter four dollar toast selfies yuccie church-key butcher kombucha DIY normcore kale chips. Street art brooklyn yuccie photo booth pok pok vice locavore.",    
+    "title": "Welcome to Camel Chicken Sunglasses!",
+    "body": "We are CCS. The only source for modern day trendy eyewear for the hipster chick that doesnt want to go into a baby boomer blood rage. Our products are 100% made in the US and set the trends for all of the industry. Whether you're a rooster or a hen, we have a style to match your needs. Click on the button to get started!",    
     "video": "https://www.youtube.com/embed/5xQD5G10W4U",
   }
-
 //taking data arrays and storing it into another array variable
-
 let indexContent = [homePage]
-
 let indexCS = {
     "homePage": indexContent
   }
-
 //Stringifying the Database to be stored in our browser for use by the controller file.
 const indexString = JSON.stringify(indexCS)
 localStorage.setItem("homePage", indexString)
