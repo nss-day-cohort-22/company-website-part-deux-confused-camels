793975962a94f6e51ce6abfc3a1207a173c5c13e
John Dulaney <j@Johns-MacBook-Pro.local>
2017-10-16 13:17:00 -0500

    index.css

--- index/index-controller.js
+++ index/index-controller.js
@@ -10,22 +10,24 @@ for (var key in indexPage) {
         for (var i = 0; i < currentItem.length; i++) {
             var detail = currentItem[i];           
 // The meat and potatoes, the raw HTML spit out after the loop has iterated its info. All plugged in to the HTML div of course.
-            indexEl.innerHTML += `
-        <section class="${key}">      
-        <div class="">
-          <img src="${detail.picture}" alt="Picture of chicken Glasses" height="600" width="900">
-          <a href="${detail.button}" class="btn btn-dark bg-warning">Shop Chicken Shades</a>  
-          <div class="">
-          <br>
-            <h1>${detail.title}</h1>
-            <p>${detail.body}</p>
-          <br>
-            <h4>A small video demonstrating the history of the chicken sunglasses:</h4>
-            <iframe width="560" height="315" src="${detail.video}" frameborder="0" allowfullscreen></iframe>
-          </div>
-        </div>    
+indexEl.innerHTML += `
+  <section class="${key}">      
+    <div class="img_containter">
+       <img src="${detail.picture}" alt="Picture of chicken Glasses" height="600" width="900">
+       <a href="${detail.button}" class="btn btn-dark bg-warning">Shop Chicken Shades</a>  
+    </div>
+<br>
+       <h1>${detail.title}</h1>
+       <p>${detail.body}</p>
+<br>
+        <h4>A small video demonstrating the history of the chicken sunglasses:</h4>
+        <iframe width="560" height="315" src="${detail.video}" frameborder="0" allowfullscreen></iframe>
+    
             `    
         }
 }
 
-
+{/* <div id="img_container">
+    <img src="http://jsfiddle.net/img/initializing.png"/>
+    <button class="button"> click here </button>
+</div> */}
