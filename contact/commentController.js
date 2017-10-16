c68b2fbd7b5cd7c7b3a829741e263cb7d1770dc3
Christopher Miller <camiller.yr@gmail.com>
2017-10-16 14:42:57 -0500

    Contact functionality

--- contact/commentController.js
+++ contact/commentController.js
@@ -4,7 +4,8 @@ let testArray=[]
 let newComment = {}
 
 // Display Comments or Emails
-function displayMessages(type, displayElement) {
+
+function displayMessages(type, displayElement, recepient) {
     document.getElementById(displayElement).innerHTML = ""
 
     let commentObjectParse = JSON.parse(localStorage.getItem("commentObjectStored"))
@@ -12,22 +13,47 @@ function displayMessages(type, displayElement) {
     //check messageType and display filter out comments / emails
     for (let index = 0; (index < commentObjectParse.length) && (index < 10); index++) {
         let element = commentObjectParse[index];
+        let docDisplay = document.getElementById(displayElement)
 
-        if (element.messageType === type) {
-            document.getElementById(displayElement).innerHTML += `
-            <div>
-            <div>${element.name}:</div>
-            <div>${element.comment}</div>
-            <div>${element.date}</div>
-            `
-            if (type === "email") {
-                document.getElementById(displayElement).innerHTML += `
-                <div><a href="mailto:${element.email}?Subject=Response%20To%20Your%20Email" target="_top">${element.email}</a></div>
-                <button onclick="deleteMessage(${index})">Delete Message</button>
+        docDisplay.innerHTML +=`<div>`
+
+        //Display messages by type - email / comment
+        if((element.messageType === "comment") || (element.messageType === "email") && (recepient === "null")) {
+        
+            if (element.messageType === type) {
+                docDisplay.innerHTML += `
+                <div>${element.name}:</div>
+                <div>${element.comment}</div>
+                <div>${element.date}</div>
                 `
-            }
+                //If email, display a mailTo link
+                if (type === "email") {
+                    docDisplay.innerHTML += `
+                    <div><a href="mailto:${element.email}?Subject=Response%20To%20Your%20Email" target="_top">${element.email}</a></div>
+                    <button onclick="deleteMessage(${index})">Delete Message</button>
+                    `
+                }
 
-            document.getElementById(displayElement).innerHTML += `<br></div>`
+                document.getElementById(displayElement).innerHTML += `<br></div>`
+            }
+        } else {
+            
+            if (element.messageType === type && element.contact === recepient) {
+                docDisplay.innerHTML += `
+                <div>${element.name}:</div>
+                <div>${element.comment}</div>
+                <div>${element.date}</div>
+                `
+                //If email, display a mailTo link
+                if (type === "email") {
+                    docDisplay.innerHTML += `
+                    <div><a href="mailto:${element.email}?Subject=Response%20To%20Your%20Email" target="_top">${element.email}</a></div>
+                    <button onclick="deleteMessage(${index})">Delete Message</button>
+                    `
+                }
+
+                document.getElementById(displayElement).innerHTML += `<br></div>`
+            }
         }
     }
     
@@ -39,12 +65,14 @@ function displayMessages(type, displayElement) {
 // Add new coment from form and add it localStorage 
 function getData() {
     
+    let v = document.getElementById("select").value
     let w = document.querySelector('input[name="messageType"]:checked').value
-    let x = document.getElementById("contactForm").elements[2].value;
-    let y = document.getElementById("contactForm").elements[3].value;
-    let z = document.getElementById("contactForm").elements[4].value;
+    let x = document.getElementById("name").value;
+    let y = document.getElementById("email").value;
+    let z = document.getElementById("comentContent").value;
     
     newComment = {
+        "contact": v,
         "messageType": w,
         "name": x,
         "email": y,
@@ -52,17 +80,17 @@ function getData() {
         "date": Date().substring(0, 24)
     }
 
+    //Add to beggining of comment array
     let commentObjectParse = JSON.parse(localStorage.getItem("commentObjectStored"))
-
     commentObjectParse.unshift(newComment)
     
+    //store array in localStorage
     let commentObjectString = JSON.stringify(commentObjectParse)
-
     localStorage.setItem("commentObjectStored", commentObjectString)
-
     document.getElementById("contactForm").reset()
+    hideSelecter()
 
-    displayMessages("comment", "commentOutput")
+    displayMessages("comment", "commentOutput", "null")
 }
 
 function myFunction() {
@@ -76,11 +104,18 @@ function myFunction() {
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
-
+// Hide Contact Selecter
+hideSelecter = function () {
+    document.getElementById("selectDiv").style.display = "none"
+}
 
 // Determines if there are stored coments and displays them or creates a localStorage item
 if (localStorage.getItem("commentObjectStored") === null) {
     localStorage.setItem("commentObjectStored", JSON.stringify(testArray))
 } else {
-    displayMessages("comment", "commentOutput")
+    displayMessages("comment", "commentOutput", "null")
 }
+
+document.getElementById("messageButton").onclick = function() {document.getElementById("selectDiv").style.display = "block"}
+
+document.getElementById("commentButton").onclick = function() {hideSelecter()}
--- contact/modal.js
+++ contact/modal.js
@@ -1,9 +1,23 @@
+let recepientVar = "null"
+
 //Login Validation
 login = function() {
     if (document.getElementById("adminLogin").elements[0].value === "camiller" && document.getElementById("adminLogin").elements[1].value === "password") {
         document.getElementById("modalTitle").innerHTML = "Emails"
         document.getElementById("modalContent").innerHTML = ""
-        displayMessages("email", "modalContent")
+        document.getElementsByClassName("modal-filter")[0].innerHTML = `
+        <div>
+            <select id="recepientSelecter"> 
+                <option value="null" selected>Sort By Recepient :</option> 
+                <option value="John" >John- Founder and CEO</option> 
+                <option value="Paul">Paul- Product Manager</option>
+                <option value="Chris">Chris- Social Media Guru</option>
+                <option value="Kristen">Kristen- Outreach Coordinator</option>
+            </select>
+            <button id="filerRecipient" onclick="filterRecepient()">Filter</button>
+        <div>
+        `
+        displayMessages("email", "modalContent", "null")
     } else {
         document.getElementById("loginFailure").innerHTML = "Username or Password are invalid"
     }
@@ -33,5 +47,9 @@ deleteMessage = function (commentIndex) {
     let commentObjectParse = JSON.parse(localStorage.getItem("commentObjectStored"))
     commentObjectParse.splice(commentIndex, 1)
     localStorage.setItem("commentObjectStored", JSON.stringify(commentObjectParse))
-    displayMessages("email", "modalContent")
+    displayMessages("email", "modalContent", "null")
+}
+
+filterRecepient = function() {
+    displayMessages("email", "modalContent", document.getElementById("recepientSelecter").value)
 }
