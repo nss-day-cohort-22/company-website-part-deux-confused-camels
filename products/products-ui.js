a0cf0dc99bc7b0a576052ddeeabe396ba180af29
Christopher Miller <camiller.yr@gmail.com>
2017-10-17 14:15:09 -0500

    paginator / products page

--- products/products-ui.js
+++ products/products-ui.js
@@ -1,35 +1,46 @@
+//pulls product list from local storage
 let list = (JSON.parse(localStorage.getItem("productsBrowserDB"))).products
-let pageList = new Array();
+
+//pageList will hold the particular items for each page depending on which page the user is on
+let pageList = [];
 let currentPage = 1;
 let numberPerPage = 4
 let numberOfPages = 1
 let pageNumberButtons = document.getElementsByClassName("paginatorPageNumber")
 
+//this function loads the initial list on the first page
 function load(){
     numberOfPages = getNumberOfPages();
     loadList()
 }
 
+// calculate number of pages by dividing the total number of items by the number of items per page
 function getNumberOfPages() {
     return Math.ceil(list.length / numberPerPage);
 }
 
+// next page will add one to the current page and load that list. nextPage is called on the onclick on the next button in the div with the id "pagination-bar" in products.html
 function nextPage() {
     currentPage += 1;
     loadList();
 }
 
+// previous page will subtract one from the current page and load that list. previousPage is called on the onclick on the previous button in the div with the id "pagination-bar" in products.html
 function previousPage() {
     currentPage -= 1;
     loadList();
 }
 
+// allows user to jump to a particular page number. pageJump is called on the onclick on the page buttons in the div with the id "pagination-bar" in products.html
 function pageJump(a) {
     currentPage = a
     loadList()
 }
 
+// this will calculate which items appear on each page
 function loadList() {
+    
+    // Enable / disable page button
     for (let index = 0; index < pageNumberButtons.length; index++) {
         if (currentPage == pageNumberButtons[index].value){
             pageNumberButtons[index].disabled = true
@@ -39,16 +50,19 @@ function loadList() {
         
     }
 
+    /*start point on array is one less than the page number multiplied by 4 (the number of items per page)*/
     let begin = ((currentPage - 1) * numberPerPage);
+    /*end point is the beginning point plus 4 (the number of items per page)*/
     let end = begin + numberPerPage;
 
+    /*this will pull the correct 4 items from the array for that page */
     pageList = list.slice(begin, end);
     drawList();    // draws out our data
     check();         // determines the states of the pagination buttons
 }
 
+// innerHTML to write to DOM for each page
 function drawList() {
-    // document.getElementById("list").innerHTML = "";
     
     document.getElementById("products-ul").innerHTML = ""
 
@@ -69,16 +83,29 @@ function drawList() {
                 <p class="product-description">${"Description: " + currentObject.description}</p>
                 <p class="product-stock">${"Current Stock: " + currentObject.stock}</p>
                 <select class="stock-select" id="${currentObject.name.replace(/ /g, "") + "Select"}" name="stock-select">${stockSelectOptions}</select>
-                <button class="buy" id="${currentObject.name.replace(/ /g, "")}">Buy Now</button>
+                <button class="buy" id="${currentObject.name.replace(/ /g, "")}" onclick="displayModal()">Buy Now</button>
             </div>
         </li>
         `
     }
 }
 
+
+//this hides the previous button on the first page and next arrow on the last page. 
 function check() {
-    document.getElementById("next").hidden = currentPage == numberOfPages ? true : false;
-    document.getElementById("previous").hidden = currentPage == 1 ? true : false;
+
+//if current page is equal to 1, then set value of hidden on the previous button to true, if not set it to false
+    document.getElementById("next").hidden = currentPage === numberOfPages ? true : false;
+ //if current page is the last page, then set the value of hidden on the next button to true, if not, set it to false
+    document.getElementById("previous").hidden = currentPage === 1 ? true : false;
+}
+
+function displayModal() {
+    document.getElementById("modal").style.display = "block"
+}
+
+document.getElementById("closeModal").onclick = function() {
+    document.getElementById('modal').style.display = "none";
 }
 
 window.onload = load()
